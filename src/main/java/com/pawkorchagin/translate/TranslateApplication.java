06a8629cb70cb4ffe5b7321803e82d2734fa3419
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-08-09 12:12:52 +0300

    Initial commit

---
+++ src/main/java/com/pawkorchagin/translate/TranslateApplication.java
@@ -0,0 +1,13 @@
+package com.pawkorchagin.translate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawkorchagin/translate/TranslateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawkorchagin.translate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
