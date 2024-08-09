e9c38edcf351c84d1448bd59cf5d457ea0ed44e1
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-08-09 22:47:35 +0300

    add echo controller

--- src/main/java/com/pawkorchagin/translate/TranslateApplication.java
+++ src/main/java/com/pawkorchagin/translate/TranslateApplication.java
@@ -3,6 +3,9 @@ package com.pawkorchagin.translate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class TranslateApplication {
 
