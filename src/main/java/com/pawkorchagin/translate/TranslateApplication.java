02eabce96bb263c2e2640f0996bf2ac42505f5b3
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-08-11 15:22:44 +0300

    add translate service via yandex translate api

--- src/main/java/com/pawkorchagin/translate/TranslateApplication.java
+++ src/main/java/com/pawkorchagin/translate/TranslateApplication.java
@@ -2,11 +2,10 @@ package com.pawkorchagin.translate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @SpringBootApplication
+@ComponentScan
 public class TranslateApplication {
 
 	public static void main(String[] args) {
