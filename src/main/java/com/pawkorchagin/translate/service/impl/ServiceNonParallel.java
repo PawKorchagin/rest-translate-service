c88724c0a2179d366ac46f95d0f99e6425f455bf
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-08-16 15:08:42 +0300

    minor changes

--- src/main/java/com/pawkorchagin/translate/service/impl/ServiceNonParallel.java
+++ src/main/java/com/pawkorchagin/translate/service/impl/ServiceNonParallel.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.pawkorchagin.translate.client.ApiClient;
-import com.pawkorchagin.translate.color.MessagePainter;
 import com.pawkorchagin.translate.model.request.YandexApiRequest;
 import com.pawkorchagin.translate.repository.UserSessionsRepository;
 import com.pawkorchagin.translate.service.IService;
@@ -47,16 +46,6 @@ public class ServiceNonParallel implements IService {
         log.info("Starting non-parallel service job");
 
         log.debug("Request Content: {}", request.toString());
-
-        if (client == null) {
-            log.error(MessagePainter.purple("ApiClient is not initialized"));
-            throw new IllegalStateException("ApiClient bean is not injected properly");
-        }
-
-        if (rep == null) {
-            log.error(MessagePainter.purple("Repository is not initialized"));
-            throw new IllegalStateException("UserSessionsRepository bean is not injected properly");
-        }
         
         ResponseEntity<?> response = client.makeRequest(request);
 
