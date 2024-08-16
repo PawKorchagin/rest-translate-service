f29011ee0b8f93f9ca4f68f070c20f75691f9947
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-08-16 15:44:40 +0300

    rename message painter

--- src/main/java/com/pawkorchagin/translate/util/MessagePainterUtil.java
+++ src/main/java/com/pawkorchagin/translate/util/MessagePainterUtil.java
@@ -1,9 +1,9 @@
-package com.pawkorchagin.translate.color;
+package com.pawkorchagin.translate.util;
 
 /**
  * MessagePainter
  */
-public class MessagePainter {
+public class MessagePainterUtil {
     public static String purple(String msg) {
         return "\u001B[35m" + msg + "\u001B[35m";
     }
